55e356b22d8b7a16dad998bea47a076dd9d606f4
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2025-02-14 01:30:04 +0530

    check deploy 3

--- app/main.py
+++ app/main.py
@@ -68,7 +68,7 @@ def weather_check(lat: Optional[float] = None, lon: Optional[float] = None):
     if lat is None or lon is None:
         raise HTTPException(
             status_code=400,
-            detail="Both 'lat' and 'lon' query parameters are required. - Check 2"
+            detail="Both 'lat' and 'lon' query parameters are required. - Check 3"
         )
 
     # Fetch weather data from OpenWeather API using the provided lat and lon
